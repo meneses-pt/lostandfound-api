ada170f9d98ad7627a521c54782e35c2b56abfec
André Meneses <andre.meneses@arpa.tech>
2020-08-17 10:24:54 +0100

    Added Item Model

--- lostandfound/models.py
+++ lostandfound/models.py
@@ -1,21 +1,58 @@
-from slugify import slugify
+from enum import Enum
 
 from app import db
-from utils import get_random_string
-from utils.models import BaseModel
+from utils.models import BaseModel, slugify_field
+
+
+class Reason(Enum):
+    looking_for = 'Looking For'
+    found = 'Found'
+
+
+class LookingForReason(Enum):
+    lost = 'Lost'
+    stolen = 'Stolen'
+    other = 'Other'
 
 
 class Category(BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    slug = db.Column(db.String(150), nullable=False, unique=True)
+    slug = db.Column(db.String(56), nullable=False, unique=True)
     parent_category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
 
-    parent_category = db.relationship('Category', backref=db.backref('child_categories', lazy='dynamic'))
+    parent_category = db.relationship('Category', backref=db.backref('child_categories', lazy='dynamic'),
+                                      remote_side=id)
 
     def __init__(self, *args, **kwargs):
-        slug = slugify(kwargs.get('name', '')) + '-' + get_random_string(5)
-        while Category.query.filter_by(slug=slug).exists():
-            slug = slugify(kwargs.get('name', '')) + '-' + get_random_string(5)
+        slug = slugify_field(kwargs.get('name', ''), 56, Category)
         kwargs['slug'] = slug
         super().__init__(*args, **kwargs)
+
+
+class Item(BaseModel):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    slug = db.Column(db.String(56), nullable=False, unique=True)
+    description = db.Column(db.String(1000), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+    reason = db.Column(db.Enum(Reason), nullable=False, index=True)
+    looking_for_reason = db.Column(db.Enum(LookingForReason), nullable=True)
+
+    category = db.relationship('Category', backref=db.backref('items', lazy='dynamic'))
+
+    __table_args__ = (db.CheckConstraint('NOT(reason = \'Looking For\' and looking_for_reason IS NULL)',
+                                         name='ck_item_looking_for_reason'),)
+
+    def __init__(self, *args, **kwargs):
+        slug = slugify_field(kwargs.get('name', ''), 56, Item)
+        kwargs['slug'] = slug
+        super().__init__(*args, **kwargs)
+
+
+class ItemImage(BaseModel):
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(36))
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+
+    item = db.relationship('Item', backref=db.backref('images', lazy='dynamic'))
--- utils/models.py
+++ utils/models.py
@@ -1,7 +1,9 @@
 from flask_jwt_extended import current_user
+from slugify import slugify
 from sqlalchemy.ext.declarative import declared_attr
 
 from app import db
+from utils import get_random_string
 
 
 class BaseModel(db.Model):
@@ -43,3 +45,11 @@ class BaseModel(db.Model):
 def _current_user_id_or_none():
     logged_user = current_user
     return logged_user.id if logged_user else None
+
+
+def slugify_field(field, max_length, model):
+    max_field_length = max_length - 6
+    slug = slugify(field[:max_field_length]) + '-' + get_random_string(5)
+    while model.query.filter_by(slug=slug).exists():
+        slug = slugify(field[:max_field_length]) + '-' + get_random_string(5)
+    return slug
